87b29a3f4277f1a3cbf0e9f83c18bad1c7c7422f
mary <123lalalala@ukr.net>
2024-01-10 21:13:28 +0200

    over

--- hw2_6.py
+++ hw2_6.py
@@ -1,34 +1,30 @@
-def get_election_winner(file_path):
-    state_results = {}
+def get_election_winner(file_path: str) -> str:
+    """
+    Знаходить переможця виборів на загальному рівні.
+
+    Параметри:
+    file_path (str): Шлях до файлу з результатами виборів.
+
+    Повертає:
+    str: Ім'я переможця виборів.
+    """
+    total_votes = {}
 
     with open(file_path, 'r') as file:
         for line in file:
-            name, votes = line.strip().split(',')
+            name, votes = line.strip().split()
 
-            if name in state_results:
-                state_results[name] += int(votes)
+            if name in total_votes:
+                total_votes[name] += int(votes)
             else:
-                state_results[name] = int(votes)
-
-    state_winners = {state: max(candidates, key=candidates.get) for state, candidates in state_results.items()}
-
-    total_votes = {}
-    for state, candidates in state_results.items():
-        for candidate, votes in candidates.items():
-            if candidate in total_votes:
-                total_votes[candidate] += votes
-            else:
-                total_votes[candidate] = votes
+                total_votes[name] = int(votes)
 
     winner = max(total_votes, key=total_votes.get)
 
-    return winner, state_winners
+    return winner
 
 
 file_path = "input.txt"
 
-
-
-president,state_winners = get_election_winner(file_path)
-print("Переможець виборів на державному рівні:", president)
-print("Переможці виборів у кожному штаті:", state_winners)
+president = get_election_winner(file_path)
+print("Переможець виборів на загальному рівні:", president)
