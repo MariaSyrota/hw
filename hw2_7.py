579a99ee597da8b5ace540e8a3b937331596f555
mary <123lalalala@ukr.net>
2024-01-23 19:12:51 +0200

    over

--- hw2_7.py
+++ hw2_7.py
@@ -1,8 +1,8 @@
 import re
 
+
 def find_most_common_shortest_word(file_path: str) -> str:
-    '''
-    Зчитує вміст файлу з вказаного шляху і знаходить найкоротше слово,
+    '''Зчитує вміст файлу з вказаного шляху і знаходить найкоротше слово,
     яке зустрічається найчастіше у тексті.
     '''
     with open(file_path, 'r') as file:
@@ -12,28 +12,38 @@ def find_most_common_shortest_word(file_path: str) -> str:
     words_count = {}
 
     for word in words:
-        if word in words_count:
-            words_count[word] += 1
-        else:
-            words_count[word] = 1
+        if len(word) > 1:
+            word = word.lower()
+            if word in words_count:
+                words_count[word] += 1
+            else:
+                words_count[word] = 1
 
-    most_common_word = min(words_count, key=lambda x: (words_count[x], len(x)))
-    return most_common_word
+    most_common_words = []
+    max_count = max(words_count.values())
 
-file_path: str = 'Input_1.txt'
-most_common_word: str = find_most_common_shortest_word(file_path)
-print("Найкоротше слово, яке зустрічається найчастіше:", most_common_word)
+    for word, count in words_count.items():
+        if count == max_count:
+            most_common_words.append(word)
+
+    shortest_word = min(most_common_words, key=len)
+    return shortest_word
 
 
 def replace_words_with_caps(file_path: str, word: str) -> None:
-    '''
-    Зчитує вміст файлу з вказаного шляху і замінює всі входження вказаного слова
+    '''Зчитує вміст файлу з вказаного шляху і замінює всі входження вказаного слова
     на великі літери.
     '''
     with open(file_path, 'r') as file:
         text = file.read()
 
-    text = text.replace(word, word.upper())
+    updated_text = re.sub(r'\b' + word + r'\b', word.upper(), text, flags=re.IGNORECASE)
 
     with open(file_path, 'w') as file:
-        file.write(text)
+        file.write(updated_text)
+
+file_path = 'Input_1.txt'
+
+most_common_word = find_most_common_shortest_word(file_path)
+print("Найкоротше слово, яке зустрічається найчастіше:", most_common_word)
+
